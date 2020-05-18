b28694e895a1732ab58aa4cf4024c9ead7d94a4a
kennentran <kennen.tran@gmail.com>
2020-05-18 22:46:50 +0700

    Initial commit

---
+++ src/sample/MainSceneController.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainSceneController {
+
+    @FXML
+    private Button productManagement;
+
+    @FXML
+    private Button orderManagement;
+
+    public void onClickProductManagement(ActionEvent event){
+
+    }
+
+    public void onClickOrderManagement(ActionEvent event){
+
+    }
+
+}
